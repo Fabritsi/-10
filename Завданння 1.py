4e2fc64359900dbd98791476c0fe172465177bfd
Yana Fabritsi <74343947+Fabritsi@users.noreply.github.com>
2020-12-31 11:21:40 +0200

    Add files via upload

---
+++ Завданння 1.py
@@ -0,0 +1,18 @@
+kanon = ...
+
+class Pryama():
+    def __init__(self, kanon):
+        print("Коефіцієнт канонічного рівняння прямої:")
+
+    def peretyn(self, ):
+
+        print("Точка перетину з іншою прямою")
+    def paralel(self, ):
+
+        print("Паралальна іншій прямій")
+    def nalej(self, ):
+
+        print("Точка належить прямій")
+
+tochky = Pryama()
+
