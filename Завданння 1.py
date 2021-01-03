e344232afe2fc0b3e99248013183ef2ee49e1165
Yana Fabritsi <74343947+Fabritsi@users.noreply.github.com>
2021-01-03 22:06:57 +0200

    Update Завданння 1.py

--- Завданння 1.py
+++ Завданння 1.py
@@ -1,18 +1,43 @@
-kanon = ...
+class SraightLine:
+    def __init__(self, a = 0, b = 0):
+        self.a = a
+        self.b = b
 
-class Pryama():
-    def __init__(self, kanon):
-        print("Коефіцієнт канонічного рівняння прямої:")
+    def vvid(self):
+        n = 3
+        print("Введіть коефіцієнти: ")
+        self.a = [int(input("Коефіцієнт a[{0}]=  ".format(i))) for i in range(1, n + 1)]
+        self.b = [int(input("Коефіцієнт b[{0}]=  ".format(i))) for i in range(1, n + 1)]
 
-    def peretyn(self, ):
+    def print_k(self):
+        print(f"StraightLine \n пряма a: {self.a}, \n пряма b :{self.b}")
 
-        print("Точка перетину з іншою прямою")
-    def paralel(self, ):
+    def peretyn(self, b):
+        d = self.a[0] * self.a[1] * self.b[1]
+        x = (self.a[0] * self.b[2] - self.b[1] * self.a[2]) / d
+        y = (self.b[0] * self.a[2] - self.a[0] * self.b[2]) / d
+        print(f'Прямі перетинаються у точці({x};{y})')
 
-        print("Паралальна іншій прямій")
-    def nalej(self, ):
+    def parallel(self,b):
+        if self.a[0]/self.b[0] == self.a[1]/self.b[1] == self.a[2]/self.b[2]:
+            print("Прямі а і b паралельні")
+        else:
+            print("Прямі а і b не паралельні")
 
-        print("Точка належить прямій")
+    def belonging(self):
+        x = float(input("x = "))
+        y = float(input("y = "))
+        if self.a[0] * x + self.a[1] * y + self.a[2] == 0:
+            print('Точка належить')
+        else:
+            print('Точка не належить')
 
-tochky = Pryama()
+
+a = SraightLine()
+b = SraightLine()
+a.input()
+a.print_k()
+a.peretyn(b)
+a.parallel(b)
+a.belonging()
 
